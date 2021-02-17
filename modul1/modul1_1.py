10339a03e242776a436fec8f48e0888fcfb10435
Emanuel Craciun <emcraciu@cisco.com>
2021-02-17 21:35:46 +0200

    modul1

---
+++ modul1/modul1_1.py
@@ -0,0 +1,29 @@
+print('Hello World')
+print('print\u200b("")')
+
+# print​("")
+print(print)
+print(3)
+
+# multiple values
+print('My name is:', 'Emanuel')
+
+# args and kwargs
+print('My name is:', 'Emanuel', '1', sep='_', end='*')
+print('My name is:', 'Emanuel', '2', sep='_', end='#')
+print('My name is:', 'Emanuel', '3', sep='_', end='@')
+
+print()
+
+print_text1 = 'Hello World 1'
+print_text2 = 'Hello World 2'
+print(print_text1, print_text2)
+
+print_text1 = 123
+print_text2 = 456
+print(print_text1, print_text2)
+
+# print shape
+print("*5*")
+print("* *")
+print("***")
