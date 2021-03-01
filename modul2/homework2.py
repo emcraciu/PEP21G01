bba91293f19fdd88ebf2e2772a49e9a48f81ee9f
emanuel.craciun <emanuel.craciun@savnet.ro>
2021-03-01 15:18:40 +0200

    homework 2

---
+++ modul2/homework2.py
@@ -0,0 +1,18 @@
+"""Homework 2 - needs to be presented before exam day"""
+
+# 20P
+# 1) Prove "and" operation takes precedence over "or" operation by setting
+# parentheses in the following expression (False or False and True or True)
+
+# 40P
+# 2) Get from input two different times in the format dd:hh:mm:ss and print
+# the difference in seconds between them. The convert the result back to the initial
+# format and print that also
+# dd is number of days
+# hh si number of hours (00-23)
+# mm is number od minutes (00-59)
+# ss is number of seconds (00-59)
+
+# 40P
+# Calculate the diagonal of a rectangle with sides 10 and 15
+
